4be0e5e89ea05fbbb63c043545e7095888a87e35
Ayushijagg <st10452981@vcconnect.edu.za>
2026-04-26 22:00:10 +0200

    Add navigation from MainActivity to AddExpenseActivity with userId intent

--- app/src/main/java/com/gia/poe_demo/MainActivity.kt
+++ app/src/main/java/com/gia/poe_demo/MainActivity.kt
@@ -2,11 +2,22 @@ package com.gia.poe_demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val userId = intent.getIntExtra("USER_ID", -1)
+
+        findViewById<CardView>(R.id.fabAddExpense).setOnClickListener {
+            val intent = Intent(this, AddExpenseActivity::class.java)
+            intent.putExtra("USER_ID", userId)
+            startActivity(intent)
+        }
     }
+
+
 }
