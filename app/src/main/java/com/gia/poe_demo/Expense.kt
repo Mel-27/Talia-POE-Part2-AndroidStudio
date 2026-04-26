6b5b05cff7d9d86b55a5a3b1e2d443ac59536412
Ayushijagg <st10452981@vcconnect.edu.za>
2026-04-26 21:54:13 +0200

    Added user to Expense entity

--- app/src/main/java/com/gia/poe_demo/Expense.kt
+++ app/src/main/java/com/gia/poe_demo/Expense.kt
@@ -6,6 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "expenses")
 data class Expense(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val userId: Int,
     val description: String,
     val amount: Double,
     val date: String
