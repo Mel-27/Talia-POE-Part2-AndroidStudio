7776baed843463e5f87696168c8a1c18f38ae87a
Mel-27 <ST10438899@vcconnect.edu.za>
2026-04-27 21:07:20 +0200

    In this commit, I added in references into my AppDatabase.kt for all the classes code

--- app/src/main/java/com/gia/poe_demo/AppDatabase.kt
+++ app/src/main/java/com/gia/poe_demo/AppDatabase.kt
@@ -12,6 +12,39 @@ import androidx.room.RoomDatabase
 )
 // This class sets up the Room database for the app and provides access to all DAOs.
 // It also ensures only one instance of the database is used throughout the app.
+
+
+/*
+References
+-Google (n.d.) Room Persistence Library.
+Available at: https://developer.android.com/training/data-storage/room
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Android Developers: Activities.
+Available at: https://developer.android.com/guide/components/activities/intro-activities
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Kotlin Coroutines on Android.
+Available at: https://developer.android.com/kotlin/coroutines
+(Accessed: 27 April 2026).
+
+-Google (n.d.) View Binding.
+Available at: https://developer.android.com/topic/libraries/view-binding
+(Accessed: 27 April 2026).
+
+-JetBrains (n.d.) Kotlin Language Documentation.
+Available at: https://kotlinlang.org/docs/home.html
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Data Access Objects (DAO) in Room.
+Available at: https://developer.android.com/training/data-storage/room/accessing-data
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Android Lifecycle and LifecycleScope.
+Available at: https://developer.android.com/topic/libraries/architecture/lifecycle
+(Accessed: 27 April 2026).
+ */
+
 abstract class AppDatabase : RoomDatabase()
 {
 
