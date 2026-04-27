92a0f12792682ecde0e5a13285bc8d41fd775c3d
Mel-27 <ST10438899@vcconnect.edu.za>
2026-04-27 21:20:50 +0200

    In this commit, I added in references in my ReminderDao.kt and BudgetGoal.kt, I also added in some comments to the classes

--- app/src/main/java/com/gia/poe_demo/MainActivity.kt
+++ app/src/main/java/com/gia/poe_demo/MainActivity.kt
@@ -13,3 +13,34 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 }
+
+/*
+References
+-Google (n.d.) Room Persistence Library.
+Available at: https://developer.android.com/training/data-storage/room
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Android Developers: Activities.
+Available at: https://developer.android.com/guide/components/activities/intro-activities
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Kotlin Coroutines on Android.
+Available at: https://developer.android.com/kotlin/coroutines
+(Accessed: 27 April 2026).
+
+-Google (n.d.) View Binding.
+Available at: https://developer.android.com/topic/libraries/view-binding
+(Accessed: 27 April 2026).
+
+-JetBrains (n.d.) Kotlin Language Documentation.
+Available at: https://kotlinlang.org/docs/home.html
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Data Access Objects (DAO) in Room.
+Available at: https://developer.android.com/training/data-storage/room/accessing-data
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Android Lifecycle and LifecycleScope.
+Available at: https://developer.android.com/topic/libraries/architecture/lifecycle
+(Accessed: 27 April 2026).
+ */
--- app/src/main/java/com/gia/poe_demo/ReminderDao.kt
+++ app/src/main/java/com/gia/poe_demo/ReminderDao.kt
@@ -20,3 +20,34 @@ interface ReminderDao {
     @Query("SELECT * FROM reminders WHERE dueDateMillis >= :fromMillis ORDER BY dueDateMillis ASC")
     fun getUpcomingReminders(fromMillis: Long): Flow<List<Reminder>>
 }
+
+/*
+References
+-Google (n.d.) Room Persistence Library.
+Available at: https://developer.android.com/training/data-storage/room
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Android Developers: Activities.
+Available at: https://developer.android.com/guide/components/activities/intro-activities
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Kotlin Coroutines on Android.
+Available at: https://developer.android.com/kotlin/coroutines
+(Accessed: 27 April 2026).
+
+-Google (n.d.) View Binding.
+Available at: https://developer.android.com/topic/libraries/view-binding
+(Accessed: 27 April 2026).
+
+-JetBrains (n.d.) Kotlin Language Documentation.
+Available at: https://kotlinlang.org/docs/home.html
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Data Access Objects (DAO) in Room.
+Available at: https://developer.android.com/training/data-storage/room/accessing-data
+(Accessed: 27 April 2026).
+
+-Google (n.d.) Android Lifecycle and LifecycleScope.
+Available at: https://developer.android.com/topic/libraries/architecture/lifecycle
+(Accessed: 27 April 2026).
+ */
